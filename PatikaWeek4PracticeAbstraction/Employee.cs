3876f72e06d8ca0c27291d8f0a360c00b74195ca
Bilal <bilalbilin12@gmail.com>
2024-08-08 16:19:24 +0300

    Add project files.

---
+++ PatikaWeek4PracticeAbstraction/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaWeek4PracticeAbstraction
+{
+    public abstract class BaseWorker // Creating an abstract  base class
+    {
+        public string Name {  get; set; } // Defining properties
+
+        public string Surname { get; set; }
+
+        public string Department    { get; set; }
+
+        public string Position { get; set; }
+
+        public abstract void Gorev(); // Creating an abstract method
+        
+    }
+
+    public class Worker : BaseWorker // Creating a new class
+    {
+        public Worker() { } // Default constructor
+
+        public Worker(string name, string surname, string department, string position) // Consructor with parameters
+        {
+            Name = name;
+            Surname = surname;
+            Department = department;
+            Position = position;
+        }
+
+        public override void Gorev() // Overriding the first method 
+        {
+            Console.WriteLine($"Ben {Name} {Surname}, bu şirketin {Department} departmanında {Position} olarak çalışıyorum.");
+        }
+
+    }
+}
---
+++ PatikaWeek4PracticeAbstraction/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using PatikaWeek4PracticeAbstraction;
+
+// Creating an instance of Worker class
+BaseWorker worker1 = new Worker("Bilal", "Bilin", "Proje Geliştirme", "Geliştirici");
+
+// Calling the method for worker1 to print informations
+worker1.Gorev();
+
+// Creating an instance of Worker class
+Worker worker2 = new Worker();
+worker2.Name = "Mikail";
+worker2.Surname = "Aslan";
+worker2.Department = "Kültür ve Sanat";
+worker2.Position = "Besteci";
+
+// Calling the method for worker2
+worker2.Gorev();
